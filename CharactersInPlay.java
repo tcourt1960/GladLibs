30a261bb72ef3feef53d1fc1ee06413266819913
Tom Courtney <tpcourtney@gmail.com>
2017-02-09 20:43:41 -0500

    macbeth actor count now works

--- CharactersInPlay.java
+++ CharactersInPlay.java
@@ -20,7 +20,15 @@ public class CharactersInPlay {
     
   public void update(String person){
   //update two arrayLists, adding the actors name if it is not there and counting
-      
+      int index = actorNames.indexOf(person);
+      if (index== -1){// found new actor
+          actorNames.add(person);
+          nameCounts.add(1);
+        }
+      else {
+          int freq = nameCounts.get(index);
+          nameCounts.set(index,freq+1);
+        }
       
   }
   
@@ -31,22 +39,42 @@ public class CharactersInPlay {
 
     FileResource resource = new FileResource();
 	for(String line : resource.lines()){
-		System.out.println(line);
+		//System.out.println(line);
 		if (line.contains(".")) {
 		    int periodIndex = line.indexOf(".");
 		    String actor = line.substring(0, periodIndex);
-		    System.out.println(actor);
+		    update(actor);
+		    //System.out.println(actor);
 		  }
 		
 	}
-  }
-  
 
+}
   
-  
+public void charactersWithNumParts(int num1, int num2){
+       for (int k=0; k<nameCounts.size(); k++){
+	    if ( (nameCounts.get(k)>=num1) && (nameCounts.get(k)<=num2)){
+        System.out.println(actorNames.get(k) + "  " + nameCounts.get(k));
+        }
+    } 
+    
+}
+
+
+
   public void tester(){
   //call findAllCharacters. print out main character followed by # of speaking parts.    
-      
+    findAllCharacters();
+    
+    for (int k=0; k<nameCounts.size(); k++){
+	    if (nameCounts.get(k)>10){
+        System.out.println(actorNames.get(k) + "  " + nameCounts.get(k));
+        }
+    }
+    int num1 = 10;
+    int num2 = 15;
+    System.out.println("Printing actors line counts between " + num1+ "  " + num2);
+    charactersWithNumParts(num1, num2);
   }
   
     
