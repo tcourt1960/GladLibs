7fc58ea66e7b29fc6d746721dc35d690b5ad47bf
Tom Courtney <tpcourtney@gmail.com>
2017-02-10 16:53:08 -0500

    starting to add Codon Count

---
+++ CodonCount.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of CodonCount here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+import edu.duke.*;
+import java.io.*;
+import java.util.*;
+
+public class CodonCount {
+
+    private HashMap<String,Integer> codonMap;
+    
+    public CodonCount(){
+        codonMap = new HashMap<String, Integer>();
+    }
+    
+public void buildCodonMap(int start, String dna){
+//builds a new map of codons mapped to their counts from string dna with reading frame with start position 0, 1, 2
+
+    
+    
+}
+    
+public String getMostCommonCodon(){
+    
+}
+
+
+}
